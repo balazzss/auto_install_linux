70ea381aba54bc50a289bc24ffcfc1cdb7f43f98
Balàzs <31547968+balazzss@users.noreply.github.com>
2020-11-15 15:56:00 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 #ajout d'une interface graphique
 whiptail --title "Bienvenue" --msgbox "Bienvenue dans l'installation des outils pour votre machine, cliquez sur OK pour continuier" 8 78
-
+root_pass=$(whiptail --nocancel --passwordbox "Pour continuer veuillez entrer le mot de passe administrateur:" 10 60 3>&1 1>&2 2>&3 3>&1)
 #Vérifier la connexion à internet, si aucune alors quitte le programme
-
-
 network () {
 whiptail --title "Internet" --msgbox "Virification si nous avons bien accès à internet" 8 78
 nc -z 8.8.8.8 53  >/dev/null 2>&1
@@ -49,45 +47,55 @@ whiptail --title "Installation des programmes" --msgbox "Installation des progra
         sudo apt install openssh-server -y
         sudo apt install fail2ban -y
         sudo apt install rsync -y
+        sudo apt install curl wget git -y
         sudo timedatectl set-timezone Europe/Brussels
 }
 
 function_changeSSHport () {
 SSHPORT=$(whiptail --inputbox "Change SSH port" 8 78  --title "Entrer le numéro du port que vous souhaitez utiliser pour vous connecter en SSH." 3>&1 1>&2 2>&3)
 echo $SSHPORT
-sed -i "s/#Port 22/Port "$SSHPORT"/g" /etc/ssh/sshd_config
+sudo sed -i "s/#Port 22/Port "$SSHPORT"/g" /etc/ssh/sshd_config
 
 if (whiptail --title "Public Key" --yesno "Voulez-vous utiliser une clé publique pour vous authentifier avec SSH?" 8 78); then
-        sed -i 's/#PubkeyAuthentication no/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
+        sudo sed -i 's/#PubkeyAuthentication no/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
 else
-        sed -i 's/#PubkeyAuthentication no/PubkeyAuthentication no/g' /etc/ssh/sshd_config
+        sudo sed -i 's/#PubkeyAuthentication no/PubkeyAuthentication no/g' /etc/ssh/sshd_config
 fi
         
 }
 
 add_user () {
-NAME=$(whiptail --inputbox "Adding user" 8 78 Name --title "Entrer le nom d'utilistaur" 3>&1 1>&2 2>&3)
-MDP=$(whiptail --passwordbox "please enter your secret password" 8 78 --title "password dialog" 3>&1 1>&2 2>&3)
+NEW_NAME=$(whiptail --inputbox "Adding user" 8 78 Name --title "Entrer le nom d'utilistaur" 3>&1 1>&2 2>&3)
 exitstatus=$?
-if [ $exitstatus = 0 ]; then
-        adduser $NAME --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
-        echo "$NAME:$MDP" | sudo chpasswd
-else
-    echo "User selected Cancel."
+if [ $exitsatus = 0 ]; then
+            #NEW_MDP=$(whiptail --passwordbox "please enter your secret password" 8 78 --title "password dialog" 3>&1 1>&2 2>&3)
+            PASS1=$(whiptail --nocancel --passwordbox "Enter a password for that user." 8 78 3>&1 1>&2 2>&3 3>&1)
+            PASS2=$(whiptail --nocancel --passwordbox "Retype password." 8 78 3>&1 1>&2 2>&3 3>&1)
+            while ! [ "$pass1" = "$pass2" ]; do
+                unset pass2
+                PASS1=$(whiptail --nocancel --passwordbox "Passwords do not match.\\n\\nEnter password again." 10 60 3>&1 1>&2 2>&3 3>&1)
+                PASS2=$(whiptail --nocancel --passwordbox "Retype password." 10 60 3>&1 1>&2 2>&3 3>&1)
+            done
+            exitstatus=$?
+            if [ $exitstatus = 0 ]; then
+                    echo "$root_pass" | sudo -S adduser $NEW_NAME --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
+                    echo "$root_pass" | sudo -S echo "$NEW_NAME:$PASS1" | sudo chpasswd
+            else
+                    echo "User selected Cancel."
+            fi
 fi
-#echo "(Exit status was $exitstatus)"
-whiptail --title "Utilisateur ajouté" --msgbox "L'utilisateur $NAME a été ajouté." 8 78
 }
 
 install_log2ram () {
 whiptail --title "Log2ram" --msgbox "Téléchargement et installation de log2ram" 8 78
-curl -Lo log2ram.tar.gz https://github.com/azlux/log2ram/archive/master.tar.gz
-tar xf log2ram.tar.gz
+sudo curl -Lo log2ram.tar.gz https://github.com/azlux/log2ram/archive/master.tar.gz
+sudo tar xf log2ram.tar.gz
 #cd log2ram-master
-chmod +x /home/$USER/log2ram-master/install.sh && sudo sh /home/$USER/log2ram-master/install.sh
+sudo chmod +x /home/$USER/log2ram-master/install.sh && sudo sh /home/$USER/log2ram-master/install.sh
 #cd ~
 sudo rm -r log2ram*
 }
+
 #lacer ces commandes sans sudo... car la commande se lance dans le dossier root.
 config_tmux () {
 whiptail --title "TMUX" --msgbox "Configuration de Tmux" 8 78
@@ -105,7 +113,7 @@ main () {
             update_upgrade || error "User exited."
             programms_install || error "User exited."
             function_changeSSHport || error "User exited."
-            adduser || error "User exited."
+            add_user || error "User exited."
             config_tmux || error "User exited."
             install_log2ram || error "User exited."
             clear
