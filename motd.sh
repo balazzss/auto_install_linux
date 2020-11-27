fa374964de8f64be9ed3b1ee736f941aca096b74
Balàzs <31547968+balazzss@users.noreply.github.com>
2020-11-27 12:35:44 +0100

    Update motd.sh

--- motd.sh
+++ motd.sh
@@ -12,11 +12,11 @@ install () {
 	          	mkdir /etc/update-motd.d
       	fi
       	# create dynamic files
-      	touch /etc/update-motd.d/00-header
-      	touch /etc/update-motd.d/10-sysinfo
-      	touch /etc/update-motd.d/90-footer
+      	sudo touch /etc/update-motd.d/00-header
+      	sudo touch /etc/update-motd.d/10-sysinfo
+      	sudo touch /etc/update-motd.d/90-footer
       	# make files executable
-      	chmod +x /etc/update-motd.d/*
+      	sudo chmod +x /etc/update-motd.d/*
       	# remove MOTD file
       	rm -r /etc/motd
 }
