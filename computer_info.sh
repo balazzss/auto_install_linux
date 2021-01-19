e09c6d12245ad263eaaea22e67102ef82e99b67a
Balàzs <31547968+balazzss@users.noreply.github.com>
2021-01-19 01:40:12 +0100

    Update computer_info.sh

--- computer_info.sh
+++ computer_info.sh
@@ -67,4 +67,3 @@ main () {
     #rm -- "$0"                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
 }                                                                                                                                                                                                                  
 main
-exit
